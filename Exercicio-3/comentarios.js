619008c6610160ca2e50069371536290e51e6590
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2024-03-19 23:10:25 -0300

    Brincando com comentários de uma ou mais linhas

---
+++ Exercicio-3/comentarios.js
@@ -0,0 +1,11 @@
+//Escrever um comentário
+console.log('Olá mundo!'); //Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+//Outro comentário
+
+/* 
+Esse aqui é um bloco de 
+comentário com mais de uma linha 
+*/
